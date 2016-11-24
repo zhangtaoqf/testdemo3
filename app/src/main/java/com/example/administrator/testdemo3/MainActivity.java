f30711208dcc4931c65836858b869a6595632b35
zhangtao <553873475@qq.com>
2016-11-24 21:44:47 +0800

    给activity添加了注释

--- app/src/main/java/com/example/administrator/testdemo3/MainActivity.java
+++ app/src/main/java/com/example/administrator/testdemo3/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.administrator.testdemo3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//今天是周四
 public class MainActivity extends AppCompatActivity {
 
     @Override
